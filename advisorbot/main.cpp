6027f65cc7e3e48d6b44acb11de4c3d66699a0e0
hikari <xxhyxayxx0619@gmail.com>
2022-05-31 19:41:14 +0900

    Initial Commit

---
+++ advisorbot/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  advisorbot
+//
+//  Created by 中村妃花理 on 2022/05/31.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
